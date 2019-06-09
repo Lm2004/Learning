d8b0a822a1983cdd8840d915a9ab96ab837133e8
Lm2004 <34040556+Lm2004@users.noreply.github.com>
2019-06-09 20:24:33 +0300

    Fixing problems.
    Fixing problems. Got another!!!

--- Coinmarketcap.py
+++ Coinmarketcap.py
@@ -13,28 +13,43 @@ driver.get('https://coinmarketcap.com/')
 
 name = list()
 website = list()
+GitHub = list()
 a=0
 m = 0
 t = 0
-while a<7:
-        a += 1
+a += 1
+
+if a<7:
+        name_1  = driver.find_elements_by_xpath('//a[@class="currency-name-container link-secondary"]')[m].text
+        name.append(name_1)
         try:
                 #better to check if the element is present?
                 driver.find_elements_by_xpath('//button[@class="banner-alert-close-button js-close"]')[0].click()
         except:
                 pass
-        driver.find_elements_by_xpath('//a[@class="currency-name-container link-secondary"]')[m].click()
-        name_1 = driver.find_elements_by_xpath('//span[@class="text-bold h3 text-gray text-large"]')[0].text
-        name.append(name_1)
-        
-        
+        m = m + 1
+        a = a + 1
+a = 0
+m = 0
+driver.get('https://coinmarketcap.com/')
+if a<7:
+        for link in driver.find_elements_by_xpath('//a[@class="currency-name-container link-secondary"]')[m]:
+                Coinlink = link.get_attribute('href')
+        website.append(Coinlink)
+        m = m + 1
+        a = a + 1
+a = 0
+m = 0
+if a<7:
         try:
+                driver.get(website[m])
                 for link in driver.find_elements_by_xpath('//a[contains(text(),"Source Code")]'):
                         link_1 = link.get_attribute('href')
                 website.append(link_1)
         except:
                 pass
-        
+        m = m + 1
+        """
         try:
                 if website[t] == website[m]: #is this for checking for duplicates? use list comprehension to avoid duplicates
                         website.pop()
@@ -46,18 +61,13 @@ while a<7:
                         pass
         except:
                 pass
+        """
 
-        m = m + 1
-        t = m - 1
-        driver.get('https://coinmarketcap.com/')
-print(name)
-print(website)
-#todo quit when not needed
+
 driver.quit()
 
-"""
-df = pd.DataFrame({'name': name ,'link': website})
+df = pd.DataFrame({'name': name ,'link': website,'GitHub link': GitHub })
 df.to_csv('Criptonic.csv', index=False, encoding='utf-8')
 print(df)
 
-"""
+
