509fbf6ae2c7d09b6eb3e5cfe62742bc9fbb6ce1
LM2004 <andvitbel@gmail.com>
2019-06-12 11:34:19 +0300

    Fixed all problems with links.

--- Coinmarketcap.py
+++ Coinmarketcap.py
@@ -3,68 +3,56 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-import lxml.etree as LE
-import requests
 import time
 import pandas as pd
 
 driver = webdriver.Firefox()
-driver.get('https://coinmarketcap.com/')
-
-name = list()
+driver.get('https://coinmarketcap.com/all/views/all/')
 website = list()
+name = list()
 GitHub = list()
-Numbers = 0
-m = 0
-t = 0
+Num = 0
+m=0
 for a in driver.find_elements_by_xpath('//a[@class="currency-name-container link-secondary"]'):
     website.append(a.get_attribute('href'))
+print("done first step")
+while Num < 2230:
+    name_1  = driver.find_elements_by_xpath('//a[@class="currency-name-container link-secondary"]')[m].text
+    name.append(name_1)
+    m = m + 1
+    Num = Num + 1
+print("done second step")
+Number = 0
+l = 0
+t = 0
+while Number < 2230: 
+    driver.get(website[l])
+    try:
+        if driver.find_element_by_xpath('//span[contains(text(),"Checking your browser before accessing")]'):
+            time.sleep(6)
+    except:
+        pass
 
-while Numbers < 100:
-        name.append(driver.find_elements_by_xpath('//a[@class="currency-name-container link-secondary"]')[m].text)
-        """
-        try:
-                #better to check if the element is present?
-                driver.find_elements_by_xpath('//button[@class="banner-alert-close-button js-close"]')[0].click()
-        except:
-                pass
+    try:
         
-        for Coinlink_get in driver.find_elements_by_xpath('//a[@class="currency-name-container link-secondary"]')[m]:
-                Coinlink = Coinlink_get.get_attribute('href')
-        website.append(Coinlink)
-        """
-        m = m + 1
-        Numbers = Numbers + 1
-
-"""
-if c<7:
-        try:
-                driver.get(website[m])
-                for GitHublink_get in driver.find_elements_by_xpath('//a[contains(text(),"Source Code")]'):
-                        GitHublink = GitHublink_get.get_attribute('href')
-                GitHub.append(GitHublink)
-        except:
-                pass
-        m = m + 1
-        c = c + 1
-        try:
-                if website[t] == website[m]: #is this for checking for duplicates? use list comprehension to avoid duplicates
-                        website.pop()
-                        driver.get('https://coinmarketcap.com/')
-                        for master in driver.find_elements_by_xpath('//a[@class="currency-name-container link-secondary"]')[m]:
-                                master_1 = link.get_attribute('href')
-                        website.append(master_1)
-                else:
-                        pass
-        except:
-                pass
-        """
+        for GitHublink_get in driver.find_elements_by_xpath('//a[contains(text(),"Source Code")]'):
+            GitHublink = (GitHublink_get.get_attribute('href'))
+        GitHub.append(GitHublink)
+    except:
+        pass
+    t = Number - 1
+    if Number > 0:
+        if GitHub[t] == GitHub[Number]:
+            GitHub.pop()
+            GitHub.append("No link")
+        else:
+            pass
+    l = l + 1
+    Number = Number + 1
 
 
-driver.quit()
-
-df = pd.DataFrame({'name': name ,'link': website})
-df.to_csv('Criptonic.csv', index=False, encoding='utf-8')
-print(df)
+data = pd.DataFrame({'name': name ,'link': website, 'GitHub': GitHub})
+data.to_csv('Criptonic.csv', index=False, encoding='utf-8')
+print(data)
 
 
