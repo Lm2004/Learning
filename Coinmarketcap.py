e5b73b126042b294d8c7e70f12cf3969b7b8a726
LM2004 <andvitbel@gmail.com>
2019-06-17 21:43:42 +0300

    Fixed problem with Github

--- Coinmarketcap.py
+++ Coinmarketcap.py
@@ -7,7 +7,7 @@ import time
 import pandas as pd
 
 driver = webdriver.Firefox()
-driver.get('https://coinmarketcap.com/all/views/all/')
+driver.get('https://coinmarketcap.com')
 website = list()
 name = list()
 GitHub = list()
@@ -16,7 +16,7 @@ m=0
 for a in driver.find_elements_by_xpath('//a[@class="currency-name-container link-secondary"]'):
     website.append(a.get_attribute('href'))
 print("done first step")
-while Num < 2230:
+while Num < 100:
     name_1  = driver.find_elements_by_xpath('//a[@class="currency-name-container link-secondary"]')[m].text
     name.append(name_1)
     m = m + 1
@@ -25,7 +25,8 @@ print("done second step")
 Number = 0
 l = 0
 t = 0
-while Number < 2230: 
+nolink = "No link"
+while Number < 100: 
     driver.get(website[l])
     try:
         if driver.find_element_by_xpath('//span[contains(text(),"Checking your browser before accessing")]'):
@@ -45,13 +46,22 @@ while Number < 2230:
         if GitHub[t] == GitHub[Number]:
             GitHub.pop()
             GitHub.append("No link")
+        elif GitHub[t] == nolink:
+            f = 0
+            while f < t:
+                if GitHublink == GitHub[f]:
+                    GitHub.pop()
+                    GitHub.append("No link")
+                else:
+                    pass
+                f = f + 1
         else:
-            pass
+            pass        
     l = l + 1
     Number = Number + 1
 
 
-data = pd.DataFrame({'name': name ,'link': website, 'GitHub': GitHub})
+data = pd.DataFrame({'name': name ,'link': website[0:100], 'GitHub': GitHub})
 data.to_csv('Criptonic.csv', index=False, encoding='utf-8')
 print(data)
 
