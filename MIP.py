1426612a781493483025377166bb348d392f8721
Lm2004 <34040556+Lm2004@users.noreply.github.com>
2019-09-08 22:21:04 +0300

    Add files via upload

--- MIP.py
+++ MIP.py
@@ -14,6 +14,7 @@ content = [x.strip() for x in content]
 sub_name = list()
 driver = webdriver.Firefox()
 xo = list()
+
 ivent_class = list()
 temp = list()
 temp_name = list()
@@ -21,7 +22,7 @@ temp_name = list()
 name = list()
 number = len(content)
 ivent = list()
-x = 3
+x = 0
 while x < number:
     driver.get(content[x])
     sleep(randint(5,7))
@@ -30,13 +31,12 @@ while x < number:
     
         for more in driver.find_elements_by_xpath("/html[1]/body[1]/div[3]/div[1]/div[4]/div[2]/div[1]/div[2]/div[1]/table[1]/tbody[1]/tr/td/div[@class='button-sm']/a[1]"):
             links = (more.get_attribute('href'))
-            xo.append(links)
+            
             temp.append(links)
     
         i = 0
         name_org = driver.find_element_by_xpath("//div[@id='content']//div[1]//div[1]//table[1]//tbody[1]//tr[3]//td[2]").text
         while i < len(temp):
-            name.append(name_org)
             temp_name.append(driver.find_elements_by_xpath("/html[1]/body[1]/div[3]/div[1]/div[4]/div[2]/div[1]/div[2]/div[1]/table[1]/tbody[1]/tr[position()>2]/td[1]")[i].text)
             i = i + 1
     
@@ -46,18 +46,23 @@ while x < number:
             driver.get(temp[l])
             sleep(randint(5,7))
             if len(driver.find_elements_by_xpath("/html[1]/body[1]/div[3]/div[1]/div[4]/div[2]/div[1]/div[1]/div[1]/table[2]/tbody[1]/tr[position()>2]/td[3]")) == 1:
+                xo.append(temp[l])
+                name.append(name_org)
                 sub_name.append(temp_name[l])
                 ivent_class.append(driver.find_element_by_xpath("/html[1]/body[1]/div[3]/div[1]/div[4]/div[2]/div[1]/div[1]/div[1]/table[2]/tbody[1]/tr[position()>2]/td[2]").text)
                 ivent.append(driver.find_element_by_xpath("/html[1]/body[1]/div[3]/div[1]/div[4]/div[2]/div[1]/div[1]/div[1]/table[2]/tbody[1]/tr[position()>2]/td[3]").text)
             elif len(driver.find_elements_by_xpath("/html[1]/body[1]/div[3]/div[1]/div[4]/div[2]/div[1]/div[1]/div[1]/table[2]/tbody[1]/tr[position()>2]/td[3]")) > 1:
-                name.pop()
+                
                 while t < len(driver.find_elements_by_xpath("/html[1]/body[1]/div[3]/div[1]/div[4]/div[2]/div[1]/div[1]/div[1]/table[2]/tbody[1]/tr[position()>2]/td[3]")):
+                    xo.append(temp[l])
                     name.append(name_org)
                     sub_name.append(temp_name[l])
                     ivent.append(driver.find_elements_by_xpath("/html[1]/body[1]/div[3]/div[1]/div[4]/div[2]/div[1]/div[1]/div[1]/table[2]/tbody[1]/tr[position()>2]/td[3]")[t].text)
                     ivent_class.append(driver.find_elements_by_xpath("/html[1]/body[1]/div[3]/div[1]/div[4]/div[2]/div[1]/div[1]/div[1]/table[2]/tbody[1]/tr[position()>2]/td[2]")[t].text)
                     t = t + 1
             else:
+                xo.append(temp[l])
+                name.append(name_org)
                 sub_name.append(temp_name[l])
                 ivent_class.append("None")
                 ivent.append("None")
@@ -73,12 +78,11 @@ while x < number:
     temp.clear()
     x = x + 1
     sleep(randint(3, 7))
-    print(name_org)
-    
-
-
-
     data = pd.DataFrame({'name': name ,'ХО': xo, 'Наименование ХО (ХП)': sub_name, 'вид РИД': ivent_class ,'Изобретение': ivent})
-    data.to_csv('MIP2.csv', index=False, encoding='utf-8')
+    data.to_csv('MIP.csv', index=False, encoding='utf-8')
     print(data)
+
+
+data = pd.DataFrame({'name': name ,'ХО': xo, 'Наименование ХО (ХП)': sub_name, 'вид РИД': ivent_class ,'Изобретение': ivent})
+data.to_csv('MIP.csv', index=False, encoding='utf-8')
 print(data)
